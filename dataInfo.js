5f14abbf406e5a7a0403e5f474ff1a96b0198a29
Sotnikov_Andrey <sav91987@ya.ru>
2024-04-18 13:45:34 +0300

    added hw_6

---
+++ dataInfo.js
@@ -0,0 +1,65 @@
+const dataInfo = `[
+    {
+        "imgUrl":"./img/SalePicture1.jpg",
+        "imgAlt":"SalePicture1",
+        "aHref":"./index2.html",
+        "imgBtnUrl":"./img/btnBasket.svg",
+        "imgBtnAlt":"btnBasket",
+        "saleCardName":"ELLERY X M'O CAPSULE",
+        "saleCardDiscript":"Known for her sculptural takes tradition tailoring, Australian arbiter of cool Kym Ellery teams up Moda Operandi.",
+        "saleCardPrice":"$52.00"
+    },
+    {
+        "imgUrl":"./img/SalePicture12.png",
+        "imgAlt":"SalePicture12",
+        "aHref":"./index2.html",
+        "imgBtnUrl":"./img/btnBasket.svg",
+        "imgBtnAlt":"btnBasket",
+        "saleCardName":"Super Clothes",
+        "saleCardDiscript":"Known for her sculptural takes Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "saleCardPrice":"$100.00"
+    },
+    {
+        "imgUrl":"./img/SalePicture3.png",
+        "imgAlt":"SalePicture3",
+        "aHref":"./index2.html",
+        "imgBtnUrl":"./img/btnBasket.svg",
+        "imgBtnAlt":"btnBasket",
+        "saleCardName":"Super Clothes XXXL",
+        "saleCardDiscript":"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi dfghjdfkgh ssfrre",
+        "saleCardPrice":"$302.00"
+    },
+    {
+        "imgUrl":"./img/SalePicture4.png",
+        "imgAlt":"SalePicture4",
+        "aHref":"./index2.html",
+        "imgBtnUrl":"./img/btnBasket.svg",
+        "imgBtnAlt":"btnBasket",
+        "saleCardName":"Супер Шмотка",
+        "saleCardDiscript":"оченьклассная одежда",
+        "saleCardPrice":"$2.00"
+    },
+    {
+        "imgUrl":"./img/SalePicture5.png",
+        "imgAlt":"SalePicture5",
+        "aHref":"./index2.html",
+        "imgBtnUrl":"./img/btnBasket.svg",
+        "imgBtnAlt":"btnBasket",
+        "saleCardName":"Трусы / носки",
+        "saleCardDiscript":"Парам папам Парам папам Парам папам Парам папам Парам папам Парам папам Парам папам",
+        "saleCardPrice":"$152.00"
+    },
+    {
+        "imgUrl":"./img/SalePicture6.png",
+        "imgAlt":"SalePicture6",
+        "aHref":"./index2.html",
+        "imgBtnUrl":"./img/btnBasket.svg",
+        "imgBtnAlt":"btnBasket",
+        "saleCardName":"Шмотка что надо",
+        "saleCardDiscript":"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "saleCardPrice":"$2000.00"
+    }
+  
+]`;
+
+console.log(dataInfo);
--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+const data = JSON.parse(dataInfo);
+
+const salesCardsEl = document.querySelector('section.salesCards');
+const salesCardTemplateEl = document.getElementById('salesCardTemplate');
+
+
+data.forEach(element => {
+	const clone = salesCardTemplateEl.content.cloneNode(true);
+
+	clone.querySelector('img.image').src = element.imgUrl;
+	clone.querySelector('img.image').alt = element.imgAlt;
+	clone.querySelector('a').href = element.aHref;
+	clone.querySelector('img.imageBtn').src = element.imgBtnUrl;
+	clone.querySelector('img.imageBtn').alt = element.imgBtnAlt;
+	clone.querySelector('p.saleCardName').innerHTML = element.saleCardName;
+	clone.querySelector('p.saleCardDiscript').innerHTML = element.saleCardDiscript;
+	clone.querySelector('p.saleCardPrice').innerHTML = element.saleCardPrice;
+
+	salesCardsEl.appendChild(clone);
+
+});
+
+
+
 const menuActive = document.querySelector('.menu-active'); 
 const burgerBtN = document.querySelector('.btnMenu');
 
@@ -7,3 +31,5 @@ function toggleMenu() {
 
 burgerBtN.addEventListener('click', toggleMenu); 
 menuClose.addEventListener('click', toggleMenu); 
+
+
